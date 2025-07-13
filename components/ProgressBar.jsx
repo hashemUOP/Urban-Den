ade5ef4881c5cf70a4a64857bf25c2e0a18c8717
hashemUOP <hashemsughaier@gmail.com>
2025-07-13 11:35:01 +0300

    almost finished getting started

---
+++ components/ProgressBar.jsx
@@ -0,0 +1,121 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React from 'react'
+import * as Progress from 'react-native-progress';
+import { Dimensions } from 'react-native';
+import CustomText from '../components/customText';
+
+const screenWidth = Dimensions.get('screen').width;
+
+export default function ProgressBar({numOfFinishedStages}) {
+  return (
+        <NumOfProgress numOfFinishedStages={numOfFinishedStages}/>
+    
+  )
+}
+function NumOfProgress({numOfFinishedStages}){
+    function Empty(){
+      return(
+            <Progress.Bar
+                progress={0}
+                width={screenWidth/7}
+                height={1.5}
+                borderWidth={1}
+                borderColor="#d6d6d4"
+                unfilledColor="#868686ff"
+                borderRadius={0}
+            />
+       ); 
+    }
+    function Filled(){
+      return(
+            <Progress.Bar
+                progress={1}
+                width={screenWidth/7}
+                color="#fefefc"  
+                height={0.5}
+                borderWidth={1}
+                borderColor="#fefefc"
+                borderRadius={0}
+            />
+       ); 
+    }
+    if(numOfFinishedStages===1){
+        return(
+            <View style={styles.progressBox}>
+                <Filled/>
+                <Empty/>
+                <Empty/>
+                <Empty/>
+                <Empty/>
+                <Pressable style={{marginLeft:"auto",marginRight:15}}>
+                    <CustomText style={{color:"#fefefc"}} >Skip</CustomText>    
+                </Pressable>
+            </View>
+        );
+    }else if(numOfFinishedStages===2){
+        return(
+            <View style={styles.progressBox}>
+                <Filled/>
+                <Filled/>
+                <Empty/>
+                <Empty/>
+                <Empty/>
+                <Pressable style={{marginLeft:"auto",marginRight:15}}>
+                    <CustomText style={{color:"#fefefc"}} >Skip</CustomText>    
+                </Pressable>
+            </View>
+        );
+    }else if(numOfFinishedStages===3){
+        return(
+            <View style={styles.progressBox}>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Empty/>
+                <Empty/>
+                <Pressable style={{marginLeft:"auto",marginRight:15}}>
+                    <CustomText style={{color:"#fefefc"}} >Skip</CustomText>    
+                </Pressable>
+            </View>
+        );
+    }else if(numOfFinishedStages===4){
+        return(
+            <View style={styles.progressBox}>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Empty/>
+                <Pressable style={{marginLeft:"auto",marginRight:15}}>
+                    <CustomText style={{color:"#fefefc"}} >Skip</CustomText>    
+                </Pressable>
+            </View>
+        );
+    }
+    return(
+            <View style={styles.progressBox}>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Filled/>
+                <Pressable style={{marginLeft:"auto",marginRight:15}}>
+                    <CustomText style={{color:"#fefefc"}} >Skip</CustomText>    
+                </Pressable>
+            </View>
+        );
+
+}
+const styles = StyleSheet.create({
+  progressBox:{
+    flexDirection:"row",
+    position: 'absolute',
+    top: 50,            
+    left: 15,              
+    right: 0,            
+    alignItems: 'center', 
+    zIndex: 10,          
+    elevation: 10,   
+    gap:5,
+  },
+});
--- pages/GetStarted.jsx
+++ pages/GetStarted.jsx
@@ -1,14 +1,25 @@
 import { View, Text , Image, StyleSheet, Dimensions} from 'react-native'
 import React from 'react'
+import * as Progress from 'react-native-progress';
+import ProgressBar from '../components/ProgressBar';
+import CustomText from '../components/customText';
+
 
 export default function GetStarted() {
   return (
-    <>
+    <View style={styles.container}>
         <Image 
-            source={require('../assets/images/get_started/michael-oxendine-GHCVUtBECuY-unsplash (2).jpg')}
+            source={require('../assets/images/get_started/denys-striyeshyn-wJ7yGwz2-00-unsplash (1).jpg')}
             style={styles.image}
         />
-    </>
+        <ProgressBar numOfFinishedStages={1}/>
+        <CustomText style={styles.title}>
+            Havely
+        </CustomText>
+        <CustomText style={styles.titleDesc}>
+            At Havely, we believe your home should be reflection of you - A haven of comfort, style, and functionality.
+        </CustomText>
+    </View>
   )
 }
 
@@ -17,10 +28,37 @@ const screenHeight = Dimensions.get('screen').height;
 
 const styles = StyleSheet.create(
     {
+        container:{
+            flex:1,
+            position:"relative"
+        },
         image:{
             width:screenWidth,
             height:screenHeight,
-            objectFit:'cover'
+            resizeMode:'cover'
+        },
+        title:{
+            position:"absolute",
+            top:screenHeight*0.15,
+            left:15,
+            fontWeight:"700",
+            color:"white",
+            fontSize:40,
+            textShadowColor: 'rgba(0, 0, 0, 0.3)',
+            textShadowOffset: { width: 1, height: 1 },
+            textShadowRadius: 4,
+        },
+        titleDesc:{
+            position:"absolute",
+            top:screenHeight*0.23,
+            left:15,
+            fontWeight:"500",
+            color:"white",
+            fontSize:16,
+            width:screenWidth*0.7,
+            textShadowColor: 'rgba(0, 0, 0, 0.5)',
+            textShadowOffset: { width: 1, height: 1 },
+            textShadowRadius: 4,
         }
     }
 );
